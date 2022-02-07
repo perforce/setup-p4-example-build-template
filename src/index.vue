acc9f9358b3531aaa261f3600ab6bc66074a475b
Andy Boutte <andyboutte@gmail.com>
2022-02-07 17:07:35 -0600

    changing comment type

--- src/index.vue
+++ src/index.vue
@@ -23,12 +23,17 @@
 						<div class="uploadBtn" @click='loadDefaultModel'>
 							<span>Default(3.5MB)</span>
 						</div>
+
 						<div class="uploadBtn" @click='loadP4ModelDark'>
 							<span>P4 asset Ship Dark (~5MB)</span>
 						</div>
-	//					<div class="uploadBtn" @click='loadP4ModelLight'>
-	//						<span>P4 asset Ship Wreck (~5MB)</span>
-	//					</div>
+
+<!---
+						<div class="uploadBtn" @click='loadP4ModelLight'>
+							<span>P4 asset Ship Wreck (~5MB)</span>
+						</div>
+--->	
+
 					</div>
 				</div>
 			</div>
